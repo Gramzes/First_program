cc409bdcbf739480b063932cc98177272906677f
Gramzes <gramzin4666@gmail.com>
2022-06-27 20:24:36 +0300

    Добавлен форматированный вывод

--- src/First_program.java
+++ src/First_program.java
@@ -1,5 +1,7 @@
 public class First_program {
-    public static void main(String[] args){
-        System.out.println("Hi!");
+    public static void main(String ...args){
+        String name = "Tom";
+        int age = 30;
+        System.out.printf("Hi! I'm %s and i'm %d years old. \n", name, age);
     }
 }
